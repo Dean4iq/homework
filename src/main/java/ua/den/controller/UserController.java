da119fd377387e7abfa58b5881b5e1d344aeba80
Denys_Onyshchenko <denis.ua22@gmail.com>
2019-05-14 14:59:06 +0300

    Commit

--- src/main/java/ua/den/controller/UserController.java
+++ src/main/java/ua/den/controller/UserController.java
@@ -1,4 +1,13 @@
 package ua.den.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
 public class UserController {
+    @RequestMapping("settings")
+    public String getSettingsPage(){
+        return "user/account_settings";
+    }
 }
